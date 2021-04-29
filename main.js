bd849731a416f8535c90541c57529303fe3ad532
Tanvi <72029176+Tanvi2010@users.noreply.github.com>
2021-04-29 19:39:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+function setup(){
+    canvas=createCanvas(400,300);
+    canvas.center();
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function find() {
+    objectdetector=ml5.objectDetector("cocossd",modelLoaded);
+    document.getElementById("status").innerHTML="Detecting Objects🔎";
+}
+function modelLoaded() {
+    console.log("Model Has Loaded🔃")
+    status=true;
+}
+
+function draw() {
+    image(video,0,0,400,300);
+}
